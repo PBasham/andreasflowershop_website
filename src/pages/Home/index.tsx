9dd44616e6c488de47ecbdaac64a37da6164bdce
patrickbasham <PatrickTBasham@gmail.com>
2023-02-05 01:33:40 -0500

    setup folder and strucutre for homepage with other components to come

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react'
+
+interface indexProps {
+
+}
+
+const index: FC<indexProps> = (props: indexProps) => {
+    const { } = props
+
+    // Hero Image
+    // About Section
+    // Services
+    // Gallary
+    return (
+        <div>
+            HomePage
+        </div>
+    )
+}
+
+export default index
