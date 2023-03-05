d1d483f7802e793575bea11e0de62be8f59dd061
patrickbasham <PatrickTBasham@gmail.com>
2023-03-04 19:47:01 -0500

    set up mobile navbar menu

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,7 +1,7 @@
 /*========================================
         Import Dependencies
 ========================================*/
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 // styling --------------------------------------------------
 import "../../assets/styling/NavBar.css"
 
@@ -12,16 +12,26 @@ interface NavBarProps {
 const NavBar: FC<NavBarProps> = (props: NavBarProps) => {
     const { } = props
 
+    const [mobileNavOpen, setMobileNavOpen] = useState<boolean>(true)
+
+    console.log(mobileNavOpen);
+
+    function onMenuToggle() {
+        console.log("I've been pressed")
+        setMobileNavOpen(!mobileNavOpen)
+    }
+
     return (
         <div id="navbar">
-            <div className="navbar-content">
-                <a className="nav-logo" href="#">Andreas Flower Shop</a>
-                <div className="nav-btns">
-                        <a className="nav-btn"  href="#" >Gallary</a>
-                        <a className="nav-btn" href="" >Contact</a>
-                        <a className="nav-btn" href="" >Get Quote</a>
-                </div>
-            </div>
+            {/* <div className="navbar-content"> */}
+                <a id="nav-logo" href="#">Andreas Flower Shop</a>
+                <div id="mobile-toggle" className={`${mobileNavOpen ? "menu-open" : ""}`} onClick={onMenuToggle}></div>
+                <ul id="nav-btns" >
+                    <a className="nav-btn" href="" >Gallary</a>
+                    <a className="nav-btn" href="" >Contact</a>
+                    <a className="nav-btn" href="" >Get Quote</a>
+                </ul>
+            {/* </div> */}
         </div>
     )
 }
