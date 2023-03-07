e82fd2c54addee0a44296122a703e1d57b32b8fc
patrickbasham <PatrickTBasham@gmail.com>
2023-03-06 19:15:14 -0500

    added base layout for services section

--- src/pages/Home/components/Services.tsx
+++ src/pages/Home/components/Services.tsx
@@ -7,13 +7,14 @@ interface ServicesProps {
 export const Services: FC<ServicesProps> = (props: ServicesProps) => {
     const { } = props
 
-    // Hero Image
-    // About Section
-    // Services
-    // Gallary
     return (
-        <div>
-            Services
+        <div id="services-section">
+
+            <div className="service-card">
+                <h3 className="service-card-header">Chair/Table Rental</h3>
+                <p className="service-card-text" >We offer xx chairs and xx tables. We something something, and something the something.</p>
+            </div>
+
         </div>
     )
 }
