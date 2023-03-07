a673657c2f727908f578c3bd2effca994832b9a4
patrickbasham <PatrickTBasham@gmail.com>
2023-03-06 19:51:32 -0500

    got base layout for gallery filter buttons

--- src/pages/Home/components/Gallary.tsx
+++ src/pages/Home/components/Gallary.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
+import { ButtonOne } from "../../../components/ui/Buttons"
 
 interface GallaryProps {
 
@@ -7,13 +8,40 @@ interface GallaryProps {
 export const Gallary: FC<GallaryProps> = (props: GallaryProps) => {
     const { } = props
 
-    // Hero Image
-    // About Section
-    // Services
-    // Gallary
+    const categories = [
+        "All",
+        "Birthday",
+        "Wedding",
+        "Anniversary",
+        "Valentines Day",
+        "Mother's Day",
+        "Sympath",
+        "Funeral",
+    ]
+
+    const [selectedCategory, setSelectedCategory] = useState("All")
+
+    /** // TODO  
+     * [] button for each category
+     * [] on button click, sort and display only those
+     * [] Unfilter button   
+    */
+
     return (
-        <div>
-            Gallary
+        <div id="gallery-section">
+            <div id="filter-btns-div">
+                {categories.map((category) => (
+                    <ButtonOne
+                        key={category}
+                        text={category}
+                        classes={"filter-btn"}
+                        onClick={() => setSelectedCategory(category)}
+                    />
+                ))}
+            </div>
+            <div id="gallery-content">
+
+            </div>
         </div>
     )
 }
